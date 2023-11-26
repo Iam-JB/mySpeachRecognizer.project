e67ac3547e702399e6eadfe2d28f24ac5d793407
jb <jbcazaux@insa-toulouse.fr>
2023-11-26 12:51:13 +0100

    Speach recognizer commands

---
+++ VoiceCommand.h
@@ -0,0 +1,30 @@
+#ifndef VOICECOMMAND_H
+#define VOICECOMMAND_H
+
+// Définition de l'ensemble des commandes vocales possibles
+#define Turn_on_the_light 1
+#define Turn_off_the_light 2
+#define PLay_music 3
+#define Pause 4
+#define Next 5
+#define Previous 6
+#define Up 7
+#define Down 8
+#define Turn_on_the_TV 9
+#define Turn_off_the_TV 10
+#define Increase_temperature 11
+#define Decrease_temperature 12
+#define What_is_the_time 13
+#define Open_the_door 14
+#define Close_the_door 15
+#define Left 16
+#define Right 17
+#define Stop 18
+#define Start 19
+#define Mode_1 20
+#define Mode_2 21
+#define Go 22
+
+int getCommand(void) ;
+
+#endif
