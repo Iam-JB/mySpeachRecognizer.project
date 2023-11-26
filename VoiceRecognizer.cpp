8c9fc97202aebf1404c42181241ddcaa48e1da8a
Iam-JB <116198831+Iam-JB@users.noreply.github.com>
2023-11-26 15:33:10 +0100

    Add files via upload
    Add VoiceRecognizer.h and VoiceRecognizer.cpp

---
+++ VoiceRecognizer.cpp
@@ -0,0 +1,18 @@
+#include <VoiceRecognizer.h>
+
+VoiceRecognizer::VoiceRecognizer(int rxPin, int txPin) : softSerial(rxPin, txPin), cmd(0) {
+    softSerial.begin(9600);
+    softSerial.listen();
+}
+
+VoiceRecognizer::~VoiceRecognizer() {
+    // Destructeur
+}
+
+int VoiceRecongizer::getCommand() {
+    if(softSerial.available())
+    {
+        cmd = softSerial.read();
+    }
+    return cmd ; // Traiter une exception si on ne passe pas dans le if
+}
