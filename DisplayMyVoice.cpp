5ff1eac4adcfb5f87a8da1dc86247f1fe5192446
Cazaux Jean-Baptiste <jbcazaux@insa-11286>
2023-11-30 11:17:44 +0100

    Ajout d'un main

---
+++ DisplayMyVoice.cpp
@@ -0,0 +1,9 @@
+#include <VoiceRecognizer.h>
+#include <LEDMatrixController.h>
+
+using namespace std ;
+
+int DisplayMyVoice() {
+  // Création d'une instance de reconnaissance de commandes vocales
+  VoiceRecognizer(2,3) ;
+}
