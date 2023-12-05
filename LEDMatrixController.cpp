8495c2418bfca43182b4a33e7bf6c88beec4a4f8
Bouquet Yann <bouquet@insa-20160>
2023-12-05 08:26:31 +0100

    maj nouvelle matrice

--- LEDMatrixController.cpp
+++ LEDMatrixController.cpp
@@ -1,5 +1,25 @@
 #include <LEDMatrixController.h>
 
+const uint64_t Emoji[] = 
+{
+  0x3c4299a581a5423c,
+  0x3c4281bd81a5423c,
+  0x3c42a59981a5423c,
+};
+const int EMOJI_LEN = sizeof(Emoji)/8;
+
+const uint64_t Carre[] = {
+  0xff000001010000ff, 0xff000003030000ff, 0xff000006060000ff,
+  0xff00000c0c0000ff, 0xff000018180000ff, 0xff000030300000ff,
+  0xff000060600000ff, 0xff0000c0c00000ff, 0xff000080800000ff,
+  0xff0000c0c00000ff, 0xff000060600000ff, 0xff000018180000ff,
+  0xff00000c0c0000ff, 0xff000006060000ff, 0xff000003030000ff,
+  0xff000001010000ff
+};
+const int CARRE_LEN = sizeof(Carre)/8;
+
+const uint64_t* animation[] = { Emoji[], Carre[] };
+
 LEDMatrixController::LEDMatrixController(VoiceRecognizer* VR) {
   this->MyVoiceRecognizer = VR;
 }
@@ -10,15 +30,10 @@ LEDMatrixController::~LEDMatrixController() {
 
 void LEDMatrixController::setupMatrix() {
     Wire.begin();
-    SERIAL.begin(115200);
-    waitForMatrixReady();
-    uint16_t VID = 0;
-    VID = matrix.getDeviceVID();
-    if (VID != 0x2886) {
-        SERIAL.println("Cannot detect led matrix !");
-        while (1);
-    }
-    SERIAL.println("Matrix init success !");
+    matrix.init();
+    matrix.setBrightness(0);
+    matrix.setBlinkRate(BLINK_OFF);
+    SERIAL.println("Matrix init success!!!");
 }
 
 void LEDMatrixController::Turn_on_the_TV(){
@@ -42,7 +57,11 @@ void LEDMatrixController::Turn_on_the_TV(){
         animation_index--; 
       }
     }
-    matrix.displayColorAnimation(animation_index, 5000, false);
+    int Animation_Len = sizeof(*Animation[animation_index])/8;
+    for (int i = 0;i < Animation_Len;i++){ // i dépend du nombre d'images de l'animation !
+        matrix.writeOnePicture(*Animation[animation_index]); // comment mettre l'animation i de l'emplacement pointé par Animation[] ?
+        delay(500);
+        }
     cmd = this.getCommand(); // il faut une instance de voice recognizer
   }
       
@@ -51,30 +70,25 @@ void LEDMatrixController::Turn_on_the_TV(){
 void LEDMatrixController::Turn_on_the_light() {
 
       int cmd = 1;
-      uint64_t Light_displayed[] = {0,0,0,0,0,0,0,0} ;
+      brightness = 0;
       
-      for (int i=0;i<8;i++){
-        Light_displayed[i]= Light[i]; // Initialisation de Light_displayed à la valeur de Light
-      }
-
-      while (cmd != Turn_off_the_light) {
+     while (cmd != Turn_off_the_light) {
         if (cmd == Decrease_temperature) {
-          if (Light_displayed[0]!=Light_min){ // on peut comparer seulement la première ligne du tableau car les valeurs de toutes les lignes sont identiques
-            for (int i=0;i<8;i++){
-              Light_displayed[i] -= Light_increment;
-            }
+          if (brightness != 0){
+           brightness-- ;
+
           }
         }
         if (cmd == Increase_temperature) { 
-          if (Light_displayed[0]!=Light_max){
-            for (int i=0;i<8;i++){
-              Light_displayed[i] += Light_increment;
-            }
+          if (brightness != 5){
+            brightness++ ; 
           }
         }
-        cmd = this.getCommand();
+        matrix.setBrightness(brightness);
+        matrix.writeOnePicture(0xffffffffffffffff);
+
+	cmd = this.getCommand();
       }
     }
         
-
-}
+ }
